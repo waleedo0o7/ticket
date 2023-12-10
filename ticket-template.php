1c1742f29632439f43143e694ba8b4bca9586e54
waleedo0o7 <waleedhd1@gmail.com>
2023-12-10 21:47:32 +0200

    finish ticket phase #1

---
+++ ticket-template.php
@@ -0,0 +1,134 @@
+<?php include 'header.php'; ?>
+
+<div class="main-page-wrapper">
+
+    <div class="container">
+
+        <div class="page-content-100">
+
+            <div class="title-wrapper mb-4">
+                <h2 class="title bold"> تفاصيل التذكرة </h2>
+            </div> <!-- title-wrapper -->
+
+            <div class="all-tickets-wrapper">
+
+                <div class="one-ticket-wrapper">
+
+                    <div class="template-wrapper">
+
+                        <div class="bg-wrapper">
+                            <img src="templates/template-01.jpg" class="img-fluid" alt="">
+                        </div>
+
+
+                        <div class="form-wrapper">
+
+                            <div class="form-group">
+
+                                <div class="title-wrapper">
+                                    <div class="en-wrapper">
+                                        <p class="en"> Name </p>
+                                    </div>
+
+                                    <div class="ar-wrapper">
+                                        <p class="ar"> الأسم </p>
+                                    </div>
+                                </div>
+
+                                <input type="text" value="waleed saeed">
+
+                            </div>
+
+
+                            <div class="tow-forms-wrapper">
+
+                                <div class="form-group w-50 d-inline left">
+
+                                    <div class="title-wrapper">
+                                        <div class="en-wrapper">
+                                            <p class="ar"> Ticket Number </p>
+                                        </div>
+
+                                        <div class="ar-wrapper">
+                                            <p class="en"> رقم التذكرة </p>
+                                        </div>
+                                    </div>
+
+                                    <input type="text" value="17963726">
+
+                                </div>
+
+
+                                <div class="form-group w-50 d-inline right">
+
+                                    <div class="title-wrapper">
+                                        <div class="en-wrapper">
+                                            <p class="en"> Ticket Type </p>
+                                        </div>
+
+                                        <div class="ar-wrapper">
+                                            <p class="ar"> نوع التذكرة </p>
+                                        </div>
+                                    </div>
+
+                                    <input type="text" value="General Admission -">
+
+                                </div>
+
+                            </div>
+
+
+                            <div class="tow-forms-wrapper">
+
+                                <div class="form-group w-50 d-inline left">
+
+                                    <div class="title-wrapper">
+                                        <div class="en-wrapper">
+                                            <p class="en"> Tour Time </p>
+                                        </div>
+
+                                        <div class="ar-wrapper">
+                                            <p class="ar"> وقت الجولة </p>
+                                        </div>
+                                    </div>
+
+                                    <input type="text" value="22:30 Mon 10 Apr 2023">
+
+                                </div>
+
+
+                                <div class="form-group w-50 d-inline right">
+
+                                    <div class="title-wrapper">
+                                        <div class="en-wrapper">
+                                            <p class="en"> Date </p>
+                                        </div>
+
+                                        <div class="ar-wrapper">
+                                            <p class="ar"> التاريخ </p>
+                                        </div>
+                                    </div>
+
+                                    <input type="text" value="10 Apr 2023">
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+                    </div> <!-- template-wrapper -->
+
+                </div><!-- one-ticket-wrapper -->
+
+            </div> <!-- tickets-wrapper -->
+
+        </div> <!-- page-content-100 -->
+
+    </div> <!-- container -->
+
+</div> <!-- main-page-wrapper -->
+
+
+<?php include 'footer.php'; ?>
